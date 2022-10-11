02cb9c8b23811932759a3d5740ce66f10a07da2d
Raihan Adam <hallwack.id@gmail.com>
2022-10-11 21:01:53 +0700

    fix: fixing loop with promise

--- index.js
+++ index.js
@@ -40,14 +40,16 @@ const questionFunc = () => {
 };
 
 const loop = new Promise((resolve, reject) => {
-  rl.on("keypress", (key) => {
-    rl.question("Apakah akan menjalankan program lagi?");
-    if (key.name == "n") {
-      reject(rl.close());
-    } else {
-      resolve("jalan");
-    }
-  })
+  setTimeout(() => {
+    resolve(questionFunc());
+  }, 500);
 });
 
-loop.then(questionFunc()).catch(rl.close());
+async function whileLooping() {
+  let state = true;
+  while (state) {
+    await loop;
+  }
+}
+
+whileLooping();
